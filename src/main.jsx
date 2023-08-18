ff29afdfe37967e610e5fb43017d8469a2d537e3
Yogesh Nikam <yogesh.nikam@ars.de>
2023-08-18 10:23:46 +0200

    Implemed the rapid API into service

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import App from './App'
+import { store } from './services/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
-    <App />
+    <Provider store={store}>
+      <App />
     </Provider>    
   </React.StrictMode>,
 )
